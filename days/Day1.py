7db7330491b8682e85082591fd50510194a04fb7
00Greenwood <marcus_greenwood@hotmail.co.uk>
2023-11-30 20:00:06 -0500

    Add Day1 Template

--- days/Day1.py
+++ days/Day1.py
@@ -1,9 +1,9 @@
 from utilities.parse import *
 from utilities.get_input import *
 
-class Day0:
+class Day1:
 	def __init__(self) -> None:
-		self.input = get_input(2023, 0)
+		self.input = get_input(2023, 1)
 		self.parsed_input = parse_int(self.input)
 		return
 	
@@ -14,7 +14,7 @@ class Day0:
 		return str(self.parsed_input)
 
 def main() -> None:
-	day = Day0()
+	day = Day1()
 	print(day.solve_part_1())
 	print(day.solve_part_2())
 	return
