0508c8c5a5f9a5a1bc0da626d400ead4276644b6
Marcus Greenwood <marcus.greenwood@autodesk.com>
2023-12-06 07:53:18 -0500

    Solve Day 6

---
+++ src/Day6.py
@@ -0,0 +1,63 @@
+from utilities.get_input import *
+from utilities.parse import *
+import time, re
+
+
+class Day6:
+    def __init__(self) -> None:
+        self.input = get_input(2023, 6)
+        self.parsed_input = parse_lines(self.input)
+        self.races = self.parse_races()
+        return
+    
+    def parse_races(self)-> list(tuple[int, int]):
+        races:list(tuple[int, int]) = []
+        times = re.findall(r'\d+', self.parsed_input[0])
+        distance = re.findall(r'\d+', self.parsed_input[1])
+        for i in range(len(times)):
+            races.append((int(times[i]), int(distance[i])))
+        return races
+
+    def solve(self, part_2: bool) -> None:
+        start_time = time.time()
+        mul: int = 1
+        if not part_2:    
+            for race_time, distance in self.races:
+                sum: int = 0
+                for speed in range(1, race_time):
+                    distance_traveled = speed * (race_time - speed)
+                    if distance_traveled > distance:
+                        sum += 1
+                mul *= sum
+        else:
+            race_time: int = 0
+            distance: int = 0
+            for races in self.races:
+                race_time = int(str(race_time) + str(races[0]))
+                distance = int(str(distance) + str(races[1]))
+            sum: int = 0
+            for speed in range(1, race_time):
+                distance_traveled = speed * (race_time - speed)
+                if distance_traveled > distance:
+                    break
+                else:
+                    sum += 1
+            for speed in range(race_time, 1, -1):
+                distance_traveled = speed * (race_time - speed)
+                if distance_traveled > distance:
+                    break
+                else:
+                    sum += 1
+            mul = race_time - sum
+        print(f'Part {'2' if part_2 else '1'}: {mul} - {time.time() - start_time}')
+
+
+def main() -> None:
+    day = Day6()
+    day.solve(False)
+    day.solve(True)
+    return
+
+
+if __name__ == "__main__":
+    main()
