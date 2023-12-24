4c03aa4496d4ba76a595522737478cedfc2c3397
00Greenwood <marcus_greenwood@hotmail.co.uk>
2023-12-23 20:21:09 -0500

    Solve Day 23 Part 2

--- src/Day23.py
+++ src/Day23.py
@@ -1,34 +1,9 @@
 from utilities.get_input import *
 from utilities.parse import *
-import time, sys
+import time
 from utilities.Grid import Grid
 from enum import Enum
-
-sys.setrecursionlimit(100000)
-
-test_input = """#.#####################
-#.......#########...###
-#######.#########.#.###
-###.....#.>.>.###.#.###
-###v#####.#v#.###.#.###
-###.>...#.#.#.....#...#
-###v###.#.#.#########.#
-###...#.#.#.......#...#
-#####.#.#.#######.#.###
-#.....#.#.#.......#...#
-#.#####.#.#.#########v#
-#.#...#...#...###...>.#
-#.#.#v#######v###.###v#
-#...#.>.#...>.>.#.###.#
-#####v#.#.###v#.#.###.#
-#.....#...#...#.#.#...#
-#.#########.###.#.#.###
-#...###...#...#...#.###
-###.###.#.###v#####v###
-#...#...#.#.>.>.#.>.###
-#.###.###.#.###.#.#v###
-#.....###...###...#...#
-#####################.#"""
+import networkx as nx
 
 type Position = tuple[int, int]
 type Hike = set[Position]
@@ -41,46 +16,66 @@ class Direction(Enum):
 
 class Day23:
     def __init__(self) -> None:
-        self.input = test_input # get_input(2023, 23)
+        self.input = get_input(2023, 23)
         self.maze = Grid(self.input)
-        self.max_hike_length = 0
+        self.start_position = (1, 0)
+        self.end_position = (self.maze.max_x - 1, self.maze.max_y)
         return
     
-    def append_to_hike_and_find_hikes(self, position: Position, end_position: Position, hike: Hike, part_2: bool) -> None:
-        hike.add(position)
-        self.find_hikes(position, end_position, hike, part_2)
-        hike.remove(position)
-
-    def find_hikes(self, position: Position, end_position: Position, hike: Hike, part_2: bool):
+    def find_intersection(self, position: Position, hike: Hike, part_2: bool) -> Position:
         for direction in Direction:
             new_position = (position[0] + direction.value[0], position[1] + direction.value[1])
             if new_position in hike:
                 continue # We have already been here
-            if new_position == end_position:
-                self.max_hike_length = max(self.max_hike_length, len(hike))
-                print(f'Current max: {self.max_hike_length}')
-                return # We have reached the end
+            if new_position == self.end_position or new_position == self.start_position:
+                hike.add(new_position) # End or start reached, we are done
+                return new_position
             symbol = self.maze.get(*new_position)
             if symbol == None or symbol == '#':
                 continue # We can not go here
             if symbol == '.':
-                self.append_to_hike_and_find_hikes(new_position, end_position, hike, part_2)
-            elif symbol == '>' and (part_2 or direction == Direction.RIGHT):
-                self.append_to_hike_and_find_hikes(new_position, end_position, hike, part_2)
-            elif symbol == '<' and (part_2 or direction == Direction.LEFT):
-                self.append_to_hike_and_find_hikes(new_position, end_position, hike, part_2)
-            elif symbol == '^' and (part_2 or direction == Direction.UP):
-                self.append_to_hike_and_find_hikes(new_position, end_position, hike, part_2)
-            elif symbol == 'v' and (part_2 or direction == Direction.DOWN):
-                self.append_to_hike_and_find_hikes(new_position, end_position, hike, part_2)
+                hike.add(new_position)
+                return self.find_intersection(new_position, hike, part_2)
+            elif symbol == '>' or symbol == '<' or symbol == '^' or symbol == 'v':
+                hike.add(new_position)
+                intersection = (new_position[0] + direction.value[0], new_position[1] + direction.value[1])
+                hike.add(intersection)
+                return intersection
+            
+    def branch(self, position: Position, graph: nx.DiGraph, part_2: bool) -> None:
+        for direction in Direction:
+            hike: Hike = {position}
+            new_position = (position[0] + direction.value[0], position[1] + direction.value[1])
+            symbol = self.maze.get(*new_position)
+            if symbol == None or symbol == '#':
+                continue # We can not go here
+            if symbol == '.' or (symbol == '>' and (part_2 or direction == Direction.RIGHT)) or (symbol == '<' and (part_2 or direction == Direction.LEFT)) or (symbol == '^' and (part_2 or direction == Direction.UP)) or (symbol == 'v' and (part_2 or direction == Direction.DOWN)):
+                hike.add(new_position)
+                intersection = self.find_intersection(new_position, hike, part_2)
+                if graph.has_edge(position, intersection):
+                    continue # We have this calculated
+                graph.add_node(intersection)
+                graph.add_edge(position, intersection, weight=len(hike))
+                self.branch(intersection, graph, part_2)
+
+    def find_longest_path(self, graph: nx.DiGraph) -> int:
+        longest_path = 0
+        for edges in nx.all_simple_edge_paths(graph, self.start_position, self.end_position):
+            path_length = 0
+            for first, second in edges:
+                # Node will be counted twice, so we need to subtract 1
+                path_length += graph.get_edge_data(first, second)['weight'] - 1
+            longest_path = max(longest_path, path_length)
+        return longest_path
+
 
     def solve(self, part_2: bool) -> None:
         start_time = time.time()
-        start_position = (1, 0)
-        end_position = (self.maze.max_x - 1, self.maze.max_y)
-        hike: set(Position) = {start_position}
-        self.find_hikes(start_position, end_position, hike, part_2)
-        print(f'Part {'2' if part_2 else '1'}: {self.max_hike_length} - {time.time() - start_time}')
+        graph = nx.DiGraph()
+        graph.add_node(self.start_position)
+        self.branch(self.start_position, graph, part_2)
+        longest_path = self.find_longest_path(graph)
+        print(f'Part {'2' if part_2 else '1'}: {longest_path} - {time.time() - start_time}')
 
 
 def main() -> None:
